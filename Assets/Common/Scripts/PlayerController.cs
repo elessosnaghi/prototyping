5ca98a69b270c1abe17a5b76637005d4addb7c62
marcosecchi <loziosecchi@gmail.com>
2020-04-23 10:27:58 +0200

    Add player controller script.

---
+++ Assets/Common/Scripts/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float rotationSpeed = 3;
+    void Update()
+    {
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
+        
+        transform.Rotate(0, hMove * rotationSpeed, 0);
+    }
+}
