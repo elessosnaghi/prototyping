7572264b7ae8e943939012e91272655187fd0059
marcosecchi <loziosecchi@gmail.com>
2020-04-23 10:43:56 +0200

    Update PlayerController.cs

--- Assets/Common/Scripts/PlayerController.cs
+++ Assets/Common/Scripts/PlayerController.cs
@@ -2,12 +2,18 @@
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3;
     public float rotationSpeed = 3;
+
     void Update()
     {
         float hMove = Input.GetAxis("Horizontal");
         float vMove = Input.GetAxis("Vertical");
         
         transform.Rotate(0, hMove * rotationSpeed, 0);
+
+        CharacterController controller = GetComponent<CharacterController>();
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        controller.SimpleMove(forward * vMove * speed);
     }
 }
