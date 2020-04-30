d61792590548d61681fc831e7b7d5e254d807088
marcosecchi <loziosecchi@gmail.com>
2020-04-30 08:52:48 +0200

    Add interaction scripts.

---
+++ Assets/Common/Scripts/ButtonActivator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ButtonActivator : MonoBehaviour
+{
+    public string buttonName = "Fire1";
+
+    public UnityEvent action;
+    
+    void Update()
+    {
+        if (Input.GetButtonDown(buttonName))
+        {
+            action.Invoke();        
+        }
+    }
+}
---
+++ Assets/Common/Scripts/Spawner.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] prefabs;
+
+    public void Spawn()
+    {
+        var prefab = prefabs[Random.Range(0, prefabs.Length)];
+        Instantiate(prefab, transform.position, transform.rotation);
+    }
+}
