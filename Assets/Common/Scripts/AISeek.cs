fd5ea6ac0bb12791e8a924b018ae6c0b8a901d2b
marcosecchi <loziosecchi@gmail.com>
2020-05-06 08:45:53 +0200

    Add AI scripts.

---
+++ Assets/Common/Scripts/AISeek.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class AISeek : MonoBehaviour
+{
+    public Transform target;
+
+    private NavMeshAgent _agent;
+    
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        _agent.destination = target.position;
+    }
+}
